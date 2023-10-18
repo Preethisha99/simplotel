98fcdd78b0fca2114a1e21e7b9b2813c36daae7a
Preethisha99 <121080603+Preethisha99@users.noreply.github.com>
2023-10-18 08:18:30 +0530

    Add files via upload

---
+++ Simpotel/assets/js/main.js
@@ -0,0 +1,10 @@
+
+    
+function myFunction() {
+    var x = document.getElementById("header");
+    if (x.className === "header") {
+        x.className += " responsive";
+    } else {
+        x.className = "header";
+    }
+}
